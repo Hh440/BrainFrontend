92f8304478749d0a33363af0d11e11f2b7af1743
Hh440 <harsh100045@gmail.com>
2024-12-09 18:15:53 +0530

    change

--- src/components/ui/SIdeBar.tsx
+++ src/components/ui/SIdeBar.tsx
@@ -11,7 +11,7 @@ export const SideBar=()=>{
     return(
         <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
             <div className="flex text-2xl pt-8 items-center">
-                <div className="pr-4  text-purple-600 cursor-pointer" onClick={()=>{navigate('/dashboard')}}>
+                <div className="pr-4  text-blue-600 cursor-pointer" onClick={()=>{navigate('/dashboard')}}>
                     <Logo/>
                 </div>
                 
